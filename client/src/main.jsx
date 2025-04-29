17f7aeb9fde5add75adbe9cd7cd1696f90b80e4b
rgcandia <alejandrogcandia@gmail.com>
2025-04-29 11:33:57 -0300

    se realiza una limpieza de componentes que viene por default, y se configura el router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+	{
+		path:"/",
+		element:<div>Hello World</div>,
+	},
+]);
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
