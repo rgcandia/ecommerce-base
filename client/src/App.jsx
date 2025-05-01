62cb68d4dd785b0dd9bd30037e956a1d0ff8467e
Ale <alejandrogcandia@gmail.com>
2025-05-01 10:05:53 -0300

    se posiciona el Nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
-
+import style from './style.module.css'
+import Nav from './components/Nav/Nav';
 function App (){
-    return(<>  
+    return(<div className={style.app} >  
+        <Nav/>
         <h1>Componente Principal  APP</h1>
-     </>)
+     </div>)
 }
 export default App;
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,6 @@
+ import style from "./Nav.module.css"
+ export default function Nav(){
+    return (<div className={style.nav}>
+    <h1 >componente NAV</h1>
+    </div>);
+ }
