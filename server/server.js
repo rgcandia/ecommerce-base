1fd6433f119c36781fd427a2a336032268ceaea1
Ale <alejandrogcandia@gmail.com>
2025-05-02 20:57:59 -0300

    se crea servidor

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*', // Permite conexiones desde cualquier origen
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
+
+module.exports = httpServer;
